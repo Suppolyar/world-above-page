ad787fb912ffa11998bcdf796213d8a7522baabc
Ilya Zakharov <74999345+Suppolyar@users.noreply.github.com>
2024-02-29 20:21:10 +0700

    fix: added scripts

--- src/App.vue
+++ src/App.vue
@@ -5,39 +5,9 @@
 </template>
 
 <script setup>
-import { onMounted } from "vue";
-import { useI18n } from "vue-i18n";
+import { init } from "./init.js";
 
-const { locale } = useI18n();
-const initLeela = () => {
-  const GEMS = window.__GEMS || {};
-  window.__GEMS = {
-    ...GEMS,
-    no_header: true,
-    lang: locale.value,
-    langs_available: [
-      "en_US",
-      "de_DE",
-      "fr_FR",
-      "es_ES",
-      "pl_PL",
-      "tr_TR",
-      "ko_KO",
-      "pt_BR",
-      "zh_CH",
-      "zh_TW",
-    ],
-    client_id: "store.my.games",
-  };
-
-  const script = document.createElement("script");
-  script.src = "//store.my.games/hotbox/leela/header.js";
-  document.head.appendChild(script);
-};
-
-onMounted(() => {
-  initLeela();
-});
+init();
 </script>
 
 <style scoped></style>
